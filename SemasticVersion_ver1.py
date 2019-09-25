f0b6c54b450a8b0d696f335f5dddd34145fa32e6
kosuke <sheep.a08@gmail.com>
2019-09-25 13:00:48 +0900

    [課題D-1]等価性の比較
    ヒントから特殊メッソドを使用
     https://docs.python.org/ja/3/reference/datamodel.html#object.__eq__

---
+++ SemasticVersion_ver1.py
@@ -0,0 +1,18 @@
+class SemanticVersion:
+    def __init__(self, major, minor, patch):
+        version = str(major) + str(minor) + str(patch)
+        self.version = 'py'+version
+
+    def __eq__(self, other):
+        return self.version == other
+
+
+def main():
+    py370 = SemanticVersion(major=3, minor=7, patch=0)
+
+    print(SemanticVersion(3, 7, 0) == py370)
+
+    print(SemanticVersion(3, 7, 1) == py370)
+
+if __name__ == '__main__':
+    main()
